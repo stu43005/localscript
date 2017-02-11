73bc8728ecec348da9a1d5ff850fa80c98348145
Shiaupiau <stu43005@gmail.com>
2017-02-11 17:23:50 +0800

    更好的支援各種模組

--- index.js
+++ index.js
@@ -1,70 +1,90 @@
-exports = module.exports = localScript;
-exports.getGlobalVariable = getGlobalVariable;
+(function(global) {
+	'use strict';
 
-function localScript(func, args) {
-	if (typeof func != "function") throw new TypeError("func is not a function");
+	var localScript = function(func, args) {
+		if (typeof func != "function") throw new TypeError("func is not a function");
 
-	var id = '__localScript__' + (new Date()).getTime();
-	var jsonArgs = JSON.stringify(args);
+		var id = '__localScript__' + (new Date()).getTime();
+		var jsonArgs = JSON.stringify(args);
 
-	var innerFunction = function(id, func, args) {
-		function trigger(name, data) {
-			var event = new CustomEvent(name, {
-				'detail': data
-			});
-			return document.dispatchEvent(event);
-		}
-		try {
-			var result = func(args);
-			trigger(id, {
-				ok: true,
-				data: JSON.stringify(result)
-			});
-		} catch (e) {
-			trigger(id, {
-				error: e.toString(),
-				data: null
-			});
-		}
-	};
-
-	var scriptText = '(' + innerFunction + ')("' + id + '", ' + func + ', ' + jsonArgs + ');';
-
-	return new Promise(function(resolve, reject) {
-		function onReceive(e) {
-			if (e.detail.error) {
-				reject(e.detail.error);
-			} else {
-				try {
-					resolve(JSON.parse(e.detail.data));
-				} catch (e) {
-					resolve();
-				}
+		var innerFunction = function(id, func, args) {
+			function trigger(name, data) {
+				var event = new CustomEvent(name, {
+					'detail': data
+				});
+				return document.dispatchEvent(event);
 			}
-			document.removeEventListener(id, onReceive);
-		}
-		document.addEventListener(id, onReceive);
+			try {
+				var result = func(args);
+				trigger(id, {
+					ok: true,
+					data: JSON.stringify(result)
+				});
+			} catch (e) {
+				trigger(id, {
+					error: e.toString(),
+					data: null
+				});
+			}
+		};
 
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.appendChild(document.createTextNode(scriptText));
-		document.body.appendChild(script);
+		var scriptText = '(' + innerFunction + ')("' + id + '", ' + func + ', ' + jsonArgs + ');';
 
-		setTimeout(function() {
-			script.parentNode.removeChild(script);
-		}, 1000);
-	});
-}
+		return new Promise(function(resolve, reject) {
+			function onReceive(e) {
+				if (e.detail.error) {
+					reject(e.detail.error);
+				} else {
+					try {
+						resolve(JSON.parse(e.detail.data));
+					} catch (e) {
+						resolve();
+					}
+				}
+				document.removeEventListener(id, onReceive);
+			}
+			document.addEventListener(id, onReceive);
 
-function getGlobalVariable(variable) {
-	return localScript(function(variable) {
-		function getValue(obj, vars) {
-			var value = obj[vars[0]];
-			if (vars.length > 1) return getValue(value, vars.slice(1));
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.appendChild(document.createTextNode(scriptText));
+			document.body.appendChild(script);
+
+			setTimeout(function() {
+				script.parentNode.removeChild(script);
+			}, 1000);
+		});
+	}
+
+	localScript.getGlobalVariable = function(variable) {
+		return localScript(function(variable) {
+			function getValue(obj, vars) {
+				var value = obj[vars[0]];
+				if (vars.length > 1) return getValue(value, vars.slice(1));
+				return value;
+			}
+
+			var value = getValue(window, variable.split('.'));
 			return value;
-		}
+		}, variable);
+	}
 
-		var value = getValue(window, variable.split('.'));
-		return value;
-	}, variable);
-}
+	// AMD support
+	if (typeof define === 'function' && define.amd) {
+		define(function() {
+			return localScript;
+		});
+		// CommonJS and Node.js module support.
+	} else if (typeof exports !== 'undefined') {
+		// Support Node.js specific `module.exports` (which can be a function)
+		if (typeof module !== 'undefined' && module.exports) {
+			exports = module.exports = localScript;
+		}
+		// But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+		exports.localScript = localScript;
+		exports.getGlobalVariable = localScript.getGlobalVariable;
+	} else {
+		global.localScript = localScript;
+		global.getGlobalVariable = localScript.getGlobalVariable;
+	}
+})(this);
